583badb3bebb16972714bbe8c2b5f2fbd60abaf5
DrkSephy <dleonar00@citymail.cuny.edu>
2015-11-09 22:02:11 -0500

    Generating list of 100 distinct integers in range [1, 10000]

---
+++ genetic.py
@@ -0,0 +1,9 @@
+# -*- genetic.py -*-
+# -*- MIT License (c) 2015 David Leonard -*-
+# -*- drksephy.github.io -*-
+
+import random
+
+# Generate a list of 100 random integers (no duplicates) 
+# in range [1, 10000]
+list = random.sample(range(1, 10000), 100)
