f5ecd179f0bbb0ad33fe98dad6560af967c34b93
DrkSephy <dleonar00@citymail.cuny.edu>
2015-11-11 22:08:39 -0500

    Scaffolding partition method for generating initial population

--- genetic.py
+++ genetic.py
@@ -5,5 +5,17 @@
 import random
 
 # Generate a list of 100 random integers (no duplicates) 
-# in range [1, 10000]
+# with range [1, 10000] exclusive
 list = random.sample(range(1, 10000), 100)
+# First population subset of 50
+subsetOne = []
+# Second population subset of 50
+subsetTwo = []
+
+# Partition method will operate on our initial list
+# and randomly partition it into two subsets each 
+# containing 50 integers, subsequently stored within
+# subsetOne and subsetTwo.
+def partition(list):
+	return
+
